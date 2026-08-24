04084cc769489e35a7813632d384c3a32ed46022
luiscleiton <luiscleitonms@gmail.com>
2026-08-23 23:17:02 -0300

    Teste de funcionalidade

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -13,7 +13,7 @@ function avaliarDesconto() {
     if (desconto < 5) {
         caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)}\nPreço final: R$ ${precoFinal.toFixed(2)}`;
         caixaResultado.classList.add('ate-5');
-    } else if (desconto = 5 && desconto <= 10) {
+    } if (desconto = 5 && desconto <= 10) {
         caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)}\nPreço final: R$ ${precoFinal.toFixed(2)}`;
         caixaResultado.classList.add('entre-5-10')
     } else {
