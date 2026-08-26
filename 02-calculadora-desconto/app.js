2fea7cb327815b8179691eea3c5a5cb5b520efd2
luiscleiton <luiscleitonms@gmail.com>
2026-08-25 23:35:12 -0300

    Resolvido: Atividade 2 - Calculadora de Desconto

--- 02-calculadora-desconto/app.js
+++ 02-calculadora-desconto/app.js
@@ -2,24 +2,33 @@
 
 const botaoCalcular = document.getElementById('calcular')
 
+function limparClasses() {
+    const caixaResultado = document.getElementById('resultado')
+    caixaResultado.classList.remove('ate-5')
+    caixaResultado.classList.remove('entre-5-10')
+    caixaResultado.classList.remove('maior-10')
+}
+
 function avaliarDesconto() {
     const preco = document.getElementById('preco-original').value
     const desconto = document.getElementById('porcentagem-desconto').value
     const caixaResultado = document.getElementById('resultado')
 
-    let valorDesconto = preco * (desconto / 100);
-    let precoFinal = preco - valorDesconto;
+    limparClasses()
 
-    if (desconto < 5) {
-        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)}\nPreço final: R$ ${precoFinal.toFixed(2)}`;
-        caixaResultado.classList.add('ate-5');
-    } if (desconto = 5 && desconto <= 10) {
-        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)}\nPreço final: R$ ${precoFinal.toFixed(2)}`;
+    let valorDesconto = preco * (desconto / 100)
+    let precoFinal = preco - valorDesconto
+
+    if (desconto <= 5) {
+        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)} | Preço final: R$ ${precoFinal.toFixed(2)}`
+        caixaResultado.classList.add('ate-5')
+    } else if (desconto <= 10) {
+        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)} | Preço final: R$ ${precoFinal.toFixed(2)}`
         caixaResultado.classList.add('entre-5-10')
     } else {
-        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)}\nPreço final: R$ ${precoFinal.toFixed(2)}`;
+        caixaResultado.textContent = `Desconto em dinheiro: R$ ${valorDesconto.toFixed(2)} | Preço final: R$ ${precoFinal.toFixed(2)}`
         caixaResultado.classList.add('maior-10')
-    } 
+    }
 }
 
 botaoCalcular.onclick = avaliarDesconto
